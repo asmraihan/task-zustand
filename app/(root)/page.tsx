d44fd3278b4b8f9464c0fc81fb1c840bc3c8e77e
asmraihan <asmraihanctg14@gmail.com>
2023-07-09 00:56:59 +0600

    root

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,28 @@
-import { Button } from "@/components/ui/button";
+import { Terminal, Waves } from "lucide-react"
 
-export default function Home() {
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert"
+
+
+export default async function Home() {
+
+  const tasks = []
+  console.log(tasks)
+
   return (
-    <div className="p-4">
-        <Button size='default' variant='outline'>Click me</Button>
-    </div>
+    <main className="p-4 max-w-4xl mx-auto h-full flex flex-col justify-center">
+      <Alert>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>NEXT TODO CRUD!</AlertTitle>
+        <AlertDescription>
+        Add, Edit, Delete, and Complete Tasks!
+        </AlertDescription>
+      </Alert>
+      <AddTask></AddTask>
+      <TodoList tasks={tasks}></TodoList>
+    </main>
   )
 }
